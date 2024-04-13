a455fff00249ad245384da06ac6a9d0f5008faa8
Tony-Ton3 <tongyuzhu1026@gmail.com>
2024-04-12 21:21:11 -0700

    started express routing to return JSON object

---
+++ src/index.mjs
@@ -0,0 +1,24 @@
+//using ES module 
+import express from 'express';
+
+const app = express();
+
+//if PORT is not defined in the environment, use 3000
+const PORT = process.env.PORT || 3000;
+
+function getTimeStamp(){
+    return new Date;
+}
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({ message: 'My name is Tony', timestamp: getTimeStamp()}));
+
+  // res.json({ message: 'My name is Tony', timestamp: getTimeStamp()});
+});
+
+//listen to port for incoming requests, this essentially starts the server
+app.listen(PORT, () => {
+  console.log(`Runninng on port ${PORT}`);
+});
+
+
