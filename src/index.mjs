fc34a8a13b29b3af26afe5e16647faadda8a2f40
Tony-Ton3 <tongyuzhu1026@gmail.com>
2024-04-17 12:28:52 -0700

    test workflow and liatrio tests

--- src/index.mjs
+++ src/index.mjs
@@ -1,23 +1,25 @@
 //using ES module 
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-const httpPort = 80;
+const curPort = process.env.PORT;
 
-function getTimeStamp(){
-    return new Date;
+function getUnixTimeStamp(){
+  return Math.floor(Date.now() / 1000);
 }
 
 app.get('/', (req, res) => {
   res.status(200).send(
-    JSON.stringify({ message: 'My name is Tony', timestamp: getTimeStamp() })
+    JSON.stringify({ message: 'My name is Tony', timestamp: getUnixTimeStamp() }, null, 2)
   );
 });
 
 //listen to port for incoming requests, this essentially starts the server
-app.listen(httpPort, () => {
-  console.log(`Runninng on port ${httpPort}`);
+app.listen(curPort, () => {
+  console.log(`Runninng on port ${curPort}`);
 });
 
 
